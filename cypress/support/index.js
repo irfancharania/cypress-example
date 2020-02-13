7f3b766849280faf58d1d636aeeae41065e269ed
Irfan Charania <irfan.charania@gmail.com>
2020-02-13 10:09:37 -0800

    Mochawesome report with screenshots

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,12 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+const addContext = require('mochawesome/addContext')
+
+Cypress.on('test:after:run', (test, runnable) => {
+    if (test.state === 'failed') {
+        const screenshotFileName = `${runnable.parent.title} -- ${test.title} (failed).png`
+        addContext({ test }, `screenshots/${Cypress.spec.name}/${screenshotFileName}`)
+    }
+})
